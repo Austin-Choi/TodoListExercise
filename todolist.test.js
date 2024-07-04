af83996f2a0b67a0dfbb1168cbd648d1aa4da256
Austin-Choi <austinsupra@gmail.com>
2024-07-05 08:40:48 +0900

    test: removeTodo 기능 테스트

--- todolist.test.js
+++ todolist.test.js
@@ -1,6 +1,6 @@
 // __tests__/todoList.test.js
 
-const { saveTodo } = require("./todolist");
+const { saveTodo, removeTodo } = require("./todolist");
 
 //describe: 테스트 케이스를 그룹화함.
 describe("saveTodo", () => {
@@ -37,5 +37,50 @@ describe("saveTodo", () => {
       "LocalStorage에서 todo를 parsing하는 중 오류가 발생했습니다.",
       expect.any(SyntaxError)
     );
+  });
+});
+
+describe("removeTodo", () => {
+  beforeEach(() => {
+    localStorage.clear();
+    jest.restoreAllMocks();
+  });
+
+  it("id를 통해 삭제할 todo를 식별하고 삭제해야 합니다", () => {
+    const initialTodos = [
+      { todoText: "Test Todo 1", id: "1" },
+      { todoText: "Test Todo 2", id: "2" },
+    ];
+    localStorage.setItem("todos", JSON.stringify(initialTodos));
+
+    removeTodo("1");
+
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    expect(todos).toHaveLength(1);
+    expect(todos[0]).toEqual({ todoText: "Test Todo 2", id: "2" });
+  });
+
+  it("JSON 파싱 에러 처리가 가능해야 합니다", () => {
+    localStorage.setItem("todos", "superbad JSON");
+
+    console.error = jest.fn();
+
+    removeTodo("1");
+
+    expect(console.error).toHaveBeenCalledWith(
+      "LocalStorage에서 todo를 parsing하는 중 오류가 발생했습니다.",
+      expect.any(SyntaxError)
+    );
+  });
+
+  it("존재하지 않는 id로 삭제하려고 하면 todos의 데이터가 불변해야 합니다", () => {
+    const initialTodos = [{ todoText: "Test Todo 1", id: "1" }];
+    localStorage.setItem("todos", JSON.stringify(initialTodos));
+
+    removeTodo("2");
+
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    expect(todos).toHaveLength(1);
+    expect(todos[0]).toEqual({ todoText: "Test Todo 1", id: "1" });
   });
 });
