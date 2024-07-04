75c353123eeaa0196bb6f80a82a7e5361a3058f8
Austin-Choi <austinsupra@gmail.com>
2024-07-04 13:28:11 +0900

    feature: 체크박스를 누르면 삭제되는 기능 추가

--- todolist.js
+++ todolist.js
@@ -5,14 +5,22 @@ const todos = document.getElementById("todos");
 inputButton.addEventListener("click", () => {
   const todoText = inputField.value.trim();
   if (todoText !== "") {
-    console.log("값이 입력됨");
     const todoItem = document.createElement("li");
     todoItem.textContent = todoText;
     todoItem.className = "todoItem";
+
     const todoItemCheckbox = document.createElement("input");
     todoItemCheckbox.type = "checkbox";
 
+    todoItemCheckbox.addEventListener("change", () => {
+      if (todoItemCheckbox.checked) {
+        todoItem.remove();
+        todoItemCheckbox.remove();
+      }
+    });
+
     todos.append(todoItem, todoItemCheckbox);
+
     inputField.value = "";
   } else {
     alert("다시 입력하세요.");
