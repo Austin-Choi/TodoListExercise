89f590dcc7df883039f09e72f18a1770ae4e3189
Austin-Choi <austinsupra@gmail.com>
2024-07-04 14:14:01 +0900

    feat: 로컬 스토리지에 todo를 배열로 저장하는 기능 추가

--- todolist.js
+++ todolist.js
@@ -2,9 +2,22 @@ const inputField = document.getElementById("todo-input-text");
 const inputButton = document.getElementById("todo-input-button");
 const todos = document.getElementById("todos");
 
+//입력받은 텍스트를 생성일자로 된 id를 생성하고
+//로컬 스토리지에서 todos를 불러옴
+//JSON으로 묶어서 todos 배열에 push함
+//다시 로컬 스토리지에 todos 배열 저장
+const saveTodo = (todoText) => {
+  const todos = JSON.parse(localStorage.getItem("todos")) || [];
+  const id = Date.now().toString();
+  todos.push({ todoText, id });
+  localStorage.setItem("todos", JSON.stringify(todos));
+  return id;
+};
+
 inputButton.addEventListener("click", () => {
   const todoText = inputField.value.trim();
   if (todoText !== "") {
+    const id = saveTodo(todoText);
     const todoItem = document.createElement("li");
     todoItem.textContent = todoText;
     todoItem.className = "todoItem";
@@ -19,15 +32,11 @@ inputButton.addEventListener("click", () => {
       }
     });
 
+    todoItem.setAttribute("todo-data-id", id);
     todos.append(todoItem, todoItemCheckbox);
 
     inputField.value = "";
   } else {
     alert("다시 입력하세요.");
   }
-});
-
-const deleteAllButton = document.getElementById("delete-all-button");
-deleteAllButton.addEventListener("click", () => {
-  localStorage.clear();
 });
